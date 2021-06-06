fe89b061c5f1eabf103a4ea7e2f60e55b06018e5
MoussaElenany95 <mousa.elenanyfciscu@gmail.com>
2021-06-06 15:10:36 +0200

    add all admin routes without auth middlware

--- Routers/Admin.js
+++ Routers/Admin.js
@@ -2,27 +2,20 @@
 const express = require("express");
 const routes = express.Router();
 const { body, validationResult } = require('express-validator');
-admins = [
-    {id:1,name:"Moussa Elenany",email:"moussa@codemodo.com",password:"12345"},
-    {id:2,name:"Afnan Wahid",email:"afnan@codemodo.com",password:"12345"},
-    {id:3,name:"Rokaia Ghareeb",email:"rokaia@codemodo.com",password:"12345"},
-    {id:4,name:"Sayed Mohamed",email:"sayed@codemodo.com",password:"12345"},
-    {id:5,name:"Eslam Gamal",email:"Eslam@codemodo.com",password:"12345"},
-    {id:6,name:"Alaa Adel",email:"alaa@codemodo.com",password:"12345"},
-    {id:7,name:"Tahany Magdy",email:"tahany@codemodo.com",password:"12345"},
-];
+const Admin = require('../Models/Admin');
+const bcrypt=require('bcrypt')
+
 /* Get All Admins*/
-routes.get('/',(req,res)=>{
+routes.get('/',async (req,res)=>{
+    const admins = await Admin.find().select('-password');
     res.send(admins);
 });
 /* Get Admin with id */
-routes.get('/:id',(req,res)=>{
-    
-    //Temp before database
-
-    const admin = admins.find(a => a.id == parseInt(req.params.id));
-    if(!admin) return res.status(404).send('Admin with givin id is not found');
-
+routes.get('/:id',async (req,res)=>{
+   
+    const admin = await Admin.findById(req.params.id).select('-password');
+    if(!admin)
+        return res.status(404).send({message:"Admin with givin id is not found"});
     return res.send(admin);
 });
 /*Add new Addmin*/
@@ -40,23 +33,79 @@ routes.post('/add',
     ,
     body('password').trim().not().isEmpty()
         .withMessage('password is required')
-    ,(req,res)=>{
-    //Errors     
+    ,async (req,res)=>{
+    //Request data errors     
     const errors = validationResult(req);
-    if (!errors.isEmpty()) return res.status(400).send({ error: errors.errors[0].msg });
+    if (!errors.isEmpty()) 
+        return res.status(400).send({ error: errors.errors[0].msg });
+    
+    adminName = req.body.name.trim();
+    adminEmail = req.body.email.trim().toLowerCase();
+    adminPassword = req.body.password;
 
-    adminId = admins.length+1;
-    adminName = req.body.name;
-    adminEmail = req.body.email;
-    addminPassword = req.body.password;
-    admins.push({id:adminId,name:adminName,email:adminEmail,password:addminPassword});
-    return res.send(`admin id is : ${req.body.name}`);
-});
-routes.put('/update/:id',(req,res)=>{
-    return res.send(`update ${req.params.id}`);
-});
-routes.delete('/delete/:id',(req,res)=>{
+    //Admin is already registered
+    if(await Admin.findOne({email:adminEmail})) 
+        return res.status(400).send({error:'Email is already taken'})
+    //Create new admin 
+    const admin = new Admin({
+        name : adminName,
+        email: adminEmail,
+        password: adminPassword
+    });
+    //Hashing password
+    const salt = await bcrypt.genSalt(10);
+    admin.password = await bcrypt.hash(admin.password,salt)
 
+    ///// save new admin
+    try{
+        await admin.save()
+        return res.send({message:'admin was registered successfully',id:admin._id}) 
+    }
+    catch(err){
+        res.send({error:'Unknown error try again later!'}) 
+    }
+
+    return res.send(admin);
+});
+//Update admin info 
+routes.put('/update/:id',async (req,res)=>{
+    try{
+        const admin = await Admin.findById(req.params.id);
+        //Check for what field will be updated
+        let updates = {
+            email: (req.body.email != "" && req.body.email != null) ? req.body.email : admin.email,
+            name: (req.body.name != "" && req.body.name != null) ? req.body.name : admin.name,
+            password: (req.body.password != "" && req.body.password != null) ? req.body.password : admin.password,
+          }
+
+        
+        if(req.body.password != "" && req.body.password != null){
+            //Hashing password
+            const salt = await bcrypt.genSalt(10);
+            updates.password = await bcrypt.hash(admin.password,salt)
+        }
+        //Saving updates
+        update = await Admin.findByIdAndUpdate(req.params.id, updates, {
+            new: true
+        });
+       
+        if(update){
+            return res.send({message:"Admin info is updated successfully"});
+        }else{
+            return res.send({message:"Unknown error try again later!"});
+        }
+      
+    }catch(err){
+        return res.status(404).send('Admin with givin id is not found');
+    }
+});
+//Delete admin info
+routes.delete('/delete/:id',async (req,res)=>{
+    const admin = await Admin.findByIdAndRemove(req.params.id);
+    if(admin){
+        return res.send({message:"Admin has been removed successfully "})
+    }
+    return res.status(404).send({message:"admin with givin id is not found"})
 });
 
 module.exports = routes;
