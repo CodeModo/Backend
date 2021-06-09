c6e0617d43a41172f9d53c34bc148b05a0b2477b
Rokaia Essam <rokaiaessam419@ci.suez.edu.eg>
2021-06-09 14:04:32 +0200

    add session schema

---
+++ Models/Classroom/Classroom.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const classroomSchema = new mongoose.Schema({
+    scheduleId: {
+        type: String,
+        required: true,
+    },
+    instructors: [String],
+    students: [String],
+    sessions: [String]
+
+},
+    { timestamps: true });
+
+const classroom = mongoose.model('Classroom', classroomSchema);
+
+module.exports = classroom;
---
+++ Models/Classroom/session.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+
+const sessionSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+    },
+    url: {
+        type: String,
+        required: true
+    },
+    comment: [
+        {
+            _id: {
+                type: mongoose.Schema.Types.ObjectId,
+                index: true,
+                required: true,
+                auto: true,
+            },
+            commenterId: {
+                type: String,
+                required: true,
+            },
+            comment: {
+                type: String,
+                required: true,
+                maxlength: 100
+            }
+        }
+    ]
+
+},
+    { timestamps: true });
+
+const session = mongoose.model('Session', sessionSchema);
+
+module.exports = session;
